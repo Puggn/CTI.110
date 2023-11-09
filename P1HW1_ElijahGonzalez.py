b9ebfa1966bb1469d51d975c37b07f4ab0bdb365
Puggn <150168880+Puggn@users.noreply.github.com>
2023-11-09 10:58:03 -0500

    P1HW1

---
+++ P1HW1_ElijahGonzalez.py
@@ -0,0 +1,26 @@
+#Elijah Gonzalez
+#11/7/2023
+#Using integers and math expresions
+
+#Get input from user
+user_number1 = int(input("Enter a integer: "))
+
+#Display outup
+print("You entered: ", user_number1)
+print(user_number1, "squared is", user_number1**2)
+print(user_number1, "cubed is", user_number1**3, "!!!")
+
+#Get input from user
+user_number2 = int(input("Enter another integer: "))
+
+#Display output
+print(user_number2)
+print(user_number1 ,"+", user_number2 ,"is", user_number1 + user_number2)
+print(user_number1 ,"*", user_number2 ,"is", user_number1 * user_number2)
+
+
+print("wow you did it, so proud of you")
+
+
+
+
